be15b2fc8086c41d25c51b9cf603cbc22dc66c45
rendalyn <z_lulu@outlook.com>
2017-03-26 08:47:13 -0500

    let's not read too much into this

--- LearningProject/main.py
+++ LearningProject/main.py
@@ -1,1 +1,24 @@
-print("test")
+import os
+
+cwd = os.getcwd()
+print(cwd)
+
+# goal: open input.txt, store numbers in a tuple
+# for every number convert it to its string
+# build a map of number->string
+
+result = []
+with open('input.txt', 'r') as f:
+    for line in f:
+        result.append(int(line))
+f.closed
+
+print(result)
+
+dict = {}
+
+for num in result:
+    dict[num] = str(num)
+
+print(dict)
+
